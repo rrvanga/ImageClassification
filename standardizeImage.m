4f20051dd05b2d02749b6438d3ec1775f08e9eb3
rrvanga <rvanga@uncc.edu>
2017-06-06 10:37:40 -0400

    upload

---
+++ standardizeImage.m
@@ -0,0 +1,7 @@
+function im = standardizeImage(im)
+% STANDARDIZEIMAGE  Rescale an image to a standard size
+%   IM = STANDARDIZEIMAGE(IM) rescale IM to have a height of at most
+%   480 pixels.
+
+im = im2single(im) ;
+if size(im,1) > 480, im = imresize(im, [480 NaN]) ; end
